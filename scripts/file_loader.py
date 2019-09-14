80cac7eb056bbfd4e1787987e86bf6e3a83318b6
JorgeAmVF <javf.pub@icloud.com>
2019-09-14 19:11:11 -0300

    Add target attribute and change enconding

--- scripts/file_loader.py
+++ scripts/file_loader.py
@@ -1,4 +1,4 @@
 def load_html(fileName):
-    with open(fileName, 'r', encoding='utf-8') as file:
+    with open(fileName, 'r', encoding='latin-1') as file:
         data = file.read().replace('  ', ' ')
     return data
